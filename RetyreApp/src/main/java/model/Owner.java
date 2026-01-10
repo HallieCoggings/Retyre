21dca8e8e7ed7e47d6c57beb3b51780c5b64b387
kermitausorusRex <162045726+kermitausorusRex@users.noreply.github.com>
2026-01-10 15:55:37 +0100

    Update Owner.java

--- RetyreApp/src/main/java/model/Owner.java
+++ RetyreApp/src/main/java/model/Owner.java
@@ -11,17 +11,18 @@ import java.util.Set;
  * <h1>Owner</h1>
  * Represents an owner of a vehicle
  */
+/*
 @Entity
 @DiscriminatorValue("Owner")
 public class Owner extends Person{
-    /* ------- PARAMETERS ------- */
+    /* ------- PARAMETERS ------- 
     @Column(name="Personal",nullable = false,length = 255)
     private String personalDetails; //TODO:Add a class "Details"
 
     @OneToMany(mappedBy = "owner")
     private Set<Vehicle> vehicles;
 
-    /* ------- CONSTRUCTOR ------- */
+    /* ------- CONSTRUCTOR -------
     //Default
     public Owner(){
     }
@@ -35,9 +36,9 @@ public class Owner extends Person{
         }
     }
 
-    /* ------- GETTER & SETTER ------- */
+    /* ------- GETTER & SETTER -------
 
-    /* ------- METHODS ------- */
+    /* ------- METHODS ------- 
     public boolean addVehicle(Vehicle v){
         if (v==null){return false;}
         if (this.vehicles.contains(v)){return false;}
@@ -78,8 +79,74 @@ public class Owner extends Person{
                 '}';
     }
 
-    /* ------- MAIN ------- */
+    /* ------- MAIN -------
     static void main() {
 
+    }
+}
+*/
+
+
+package model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <h1>Owner</h1>
+ * Represents an owner of one or more vehicles.
+ * Inherits from Person
+ */
+@Entity
+@Table(name = "owner")
+@DiscriminatorValue("OWNER")
+public class Owner extends Person {
+
+    /* ------- PARAMETERS ------- */
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Vehicle> vehicles = new HashSet<>();
+
+    /* ------- CONSTRUCTORS ------- */
+    public Owner() {
+        super();
+    }
+
+    public Owner(String name, String firstName) {
+        super(name, firstName);
+    }
+
+    public Owner(String name, String firstName, LocalDate dateOfBirth, String telephone) {
+        super(name, firstName, dateOfBirth, telephone);
+    }
+
+    /* ------- GETTERS AND SETTERS ------- */
+    public Set<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(Set<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    /* ------- METHODS ------- */
+    public void addVehicle(Vehicle vehicle) {
+        if (vehicle != null) {
+            this.vehicles.add(vehicle);
+            vehicle.setOwner(this); //TODO: check if name is correct in Vehicle
+        }
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+        if (vehicle != null) {
+            this.vehicles.remove(vehicle);
+            vehicle.setOwner(null);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{id=" + getId() + ", name='" + getFullName() + "', vehicles=" + vehicles.size() + "}";
     }
 }
