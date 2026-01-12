10d8e2bf578e3ad6e9a87b69061175e550d3da12
HallieCogging <162910692+HallieCoggings@users.noreply.github.com>
2026-01-12 20:49:11 +0100

    feat: Begin adding elements to Header and body
    This an early development this elements will probably  be modify in next commit

--- RetyreApp/src/main/java/gui/Body.java
+++ RetyreApp/src/main/java/gui/Body.java
@@ -4,8 +4,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Body extends JPanel {
+    private BorderLayout layout;
+    private JScrollPane futureIntervention;
+    private JPanel calendar;
+
     public Body(){
         super();
         this.setBackground(Color.pink);
+        this.layout = new BorderLayout();
+        this.futureIntervention = new JScrollPane();
+        this.calendar = new JPanel();
+
+        this.futureIntervention.setBackground(Color.blue);
+        this.futureIntervention.setPreferredSize(new Dimension(1000,700));
+        this.setLayout(this.layout);
+        this.calendar.setBackground(Color.blue);
+        this.calendar.setPreferredSize(new Dimension(200,700));
+
+        this.add(futureIntervention,BorderLayout.WEST);
+        this.add(calendar,BorderLayout.EAST);
     }
 }
--- RetyreApp/src/main/java/gui/Header.java
+++ RetyreApp/src/main/java/gui/Header.java
@@ -6,11 +6,25 @@ import java.awt.*;
 public class Header extends JPanel {
     private Dimension dimension;
     private GridLayout layout;
+    private JButton addButton;
+    private JButton searchButton;
+    private JButton interventionButton;
+
     public Header(){
         super();
         this.dimension = new Dimension(300,100);
         this.setBackground(Color.green);
         this.setPreferredSize(this.dimension);
-        this.layout = new GridLayout(2,4);
+        this.layout = new GridLayout(1,4);
+        this.setLayout(this.layout);
+        this.addButton = new JButton("Add Vehicle");
+        this.searchButton = new JButton("Search a vehicle");
+        this.interventionButton = new JButton("Create an Intervnetion");
+
+        this.addButton.addActionListener(); //TODO : Regarder comment implémente les actions listeners
+
+        this.add(addButton);
+        this.add(searchButton);
+        this.add((interventionButton));
     }
 }
