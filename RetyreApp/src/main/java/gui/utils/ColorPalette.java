0fecec7992891689bc592c3a0f8f5750688463e9
kermitausorusRex <162045726+kermitausorusRex@users.noreply.github.com>
2026-01-14 09:10:33 +0100

    Update ColorPalette.java

--- RetyreApp/src/main/java/gui/utils/ColorPalette.java
+++ RetyreApp/src/main/java/gui/utils/ColorPalette.java
@@ -5,8 +5,9 @@ import java.awt.Color;
 public class ColorPalette {
 
     public static final Color DARK_BLUE = new Color(57, 76, 96);
-    public static final Color LIGHT_GRAY = new Color(226 , 226 , 226);
-    public static final Color GREEN_BUTTON = new Color(126, 217, 87);
+    public static final Color LIGHT_GREY = new Color(226 , 226 , 226);
+    
+    //Intervention state
     public static final Color RED_CRITICAL = new Color(255, 49, 49);
     public static final Color ORANGE_URGENT = new Color(255, 145, 77);
     public static final Color YELLOW_APPROACHING = new Color(255, 193, 7);
@@ -14,18 +15,18 @@ public class ColorPalette {
 
 
     // Intervention colors
-    public static final Color INTERVENTION_NORMAL_BG = new Color(200, 255, 200);
-    public static final Color INTERVENTION_NORMAL_FG = new Color(0, 100, 0);
-    public static final Color INTERVENTION_URGENT_BG = new Color(255, 200, 200);
-    public static final Color INTERVENTION_URGENT_FG = new Color(139, 0, 0);
+    public static final Color INTERVENTION_NORMAL_BG = new Color(200, 255, 200); //bg
+    public static final Color INTERVENTION_NORMAL_F = new Color(0, 100, 0); //font
+    public static final Color INTERVENTION_URGENT_BG = new Color(255, 200, 200); //bg
+    public static final Color INTERVENTION_URGENT_F = new Color(139, 0, 0); //font
 
     //Table header
-    public static final Color TABLE_HEADER_BG = new Color(80, 80, 80);   // Gris foncé
-    public static final Color TABLE_HEADER_FG = Color.WHITE;
+    public static final Color TABLE_HEADER_BG = new Color(80, 80, 80);
+    public static final Color TABLE_HEADER_F = Color.WHITE;
 
     //Calendar day dots
-    public static final Color CALENDAR_AVAILABLE = new Color(144, 238, 144);  // Vert clair
-    public static final Color CALENDAR_BUSY = new Color(255, 99, 71);         // Rouge tomate
+    public static final Color CALENDAR_AVAILABLE = GREEN_OK;
+    public static final Color CALENDAR_BUSY = RED_CRITICAL;
 
     //Text font color
     public static final Color WHITE_TEXT = Color.WHITE;
@@ -36,18 +37,16 @@ public class ColorPalette {
     public static final Color BACKGROUND_WHITE = Color.WHITE;
     public static final Color BACKGROUND_LIGHT = new Color(245, 245, 245);
     public static final Color BACKGROUND_DARK = new Color(60, 60, 60);
-
-    //Border
-    public static final Color BORDER_GRAY = new Color(180, 180, 180);
-
+    
     //BUttons
     public static final Color WHITE_BUTTON = new Color(240, 240, 240);
+    public static final Color GREEN_BUTTON = new Color(126, 217, 87);
 
     //Navigation (on top of main frame)
     public static final Color NAV_BUTTON_BG = Color.BLACK;
     public static final Color NAV_BUTTON_HOVER = new Color(40, 40, 40);
 
-    //Highlighting parts or buttons
+    //Highlighting parts
     public static final Color HIGHLIGHT_YELLOW = new Color(255, 255, 200);
     public static final Color HOVER_BLUE = new Color(200, 220, 255);
 }
